1a9df98b64585c232bfd7dabd321adfa90a82481
tooler <1836783249@qq.com>
2020-09-15 17:57:29 +0800

    文件管理系统页面； 学习点：用户组件-赋值

---
+++ FileManager/UCmediumSizeButtons.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileManager
+{
+    /// <summary>
+    /// UCmediumSizeButtons.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCmediumSizeButtons : UserControl
+    {
+        public UCmediumSizeButtons()
+        {
+            InitializeComponent();
+        }
+        public ImageSource imageSource
+        {
+            get
+            {
+                return buttonImage.Source;
+            }
+            set
+            {
+                buttonImage.Source = value;
+            }
+        }
+
+        public string text1
+        {
+            get
+            {
+                return Text1.Text;
+            }
+            set
+            {
+                Text1.Text = value;
+            }
+        }  
+        public string text2
+        {
+            get
+            {
+                return Text2.Text;
+            }
+            set
+            {
+                Text2.Text = value;
+            }
+        }
+    }
+}
