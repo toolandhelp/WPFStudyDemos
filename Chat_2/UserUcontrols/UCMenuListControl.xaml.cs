b5ba1301242876122159832f68e1c471957cf196
tooler <1836783249@qq.com>
2020-09-18 18:31:02 +0800

    添加一个聊天页面 学习于：https://www.youtube.com/watch?v=KywohYdFZjI

---
+++ Chat_2/UserUcontrols/UCMenuListControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Chat_2.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chat_2.UserUcontrols
+{
+    /// <summary>
+    /// UCMenuListControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCMenuListControl : UserControl
+    {
+        public UCMenuListControl()
+        {
+            InitializeComponent();
+
+            DataContext = new ViewModel();
+        }
+    }
+}
