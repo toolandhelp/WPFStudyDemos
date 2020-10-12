33f5079889432e640c2bf92edecdb8ac0457eeb6
tooler <1836783249@qq.com>
2020-10-12 16:51:02 +0800

    动态加载动画

---
+++ Demo/UserControls/LoadingWait.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Demo.UserControls
+{
+    /// <summary>
+    /// LoadingWait.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoadingWait : UserControl
+    {
+        private readonly DispatcherTimer animationTimer;
+        public LoadingWait()
+        {
+            InitializeComponent();
+
+            animationTimer = new DispatcherTimer(
+              DispatcherPriority.ContextIdle, Dispatcher);
+            animationTimer.Interval = new TimeSpan(0, 0, 0, 0, 90);
+        }
+
+        #region Private Methods  
+        private void Start()
+        {
+            animationTimer.Tick += HandleAnimationTick;
+            animationTimer.Start();
+        }
+
+        private void Stop()
+        {
+            animationTimer.Stop();
+            animationTimer.Tick -= HandleAnimationTick;
+        }
+
+        private void HandleAnimationTick(object sender, EventArgs e)
+        {
+            SpinnerRotate.Angle = (SpinnerRotate.Angle + 36) % 360;
+        }
+
+        private void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            const double offset = Math.PI;
+            const double step = Math.PI * 2 / 10.0;
+
+            SetPosition(C0, offset, 0.0, step);
+            SetPosition(C1, offset, 1.0, step);
+            SetPosition(C2, offset, 2.0, step);
+            SetPosition(C3, offset, 3.0, step);
+            SetPosition(C4, offset, 4.0, step);
+            SetPosition(C5, offset, 5.0, step);
+            SetPosition(C6, offset, 6.0, step);
+            SetPosition(C7, offset, 7.0, step);
+            SetPosition(C8, offset, 8.0, step);
+        }
+
+        private void SetPosition(Ellipse ellipse, double offset,
+            double posOffSet, double step)
+        {
+            ellipse.SetValue(Canvas.LeftProperty, 50.0
+                + Math.Sin(offset + posOffSet * step) * 50.0);
+
+            ellipse.SetValue(Canvas.TopProperty, 50
+                + Math.Cos(offset + posOffSet * step) * 50.0);
+        }
+
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            Stop();
+        }
+
+        private void HandleVisibleChanged(object sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            bool isVisible = (bool)e.NewValue;
+
+            if (isVisible)
+                Start();
+            else
+                Stop();
+        }
+        #endregion
+
+
+        #region 传入属性
+
+        /// <summary>
+        /// 不透明颜色
+        /// </summary>
+        public SolidColorBrush OpacityColor
+        {
+            get { return (SolidColorBrush)GetValue(OpacityColorProperty); }
+            set { SetValue(OpacityColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OpacityColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OpacityColorProperty =
+            DependencyProperty.Register("OpacityColor", typeof(SolidColorBrush), typeof(LoadingWait), new FrameworkPropertyMetadata(Brushes.DimGray));
+
+
+        /// <summary>
+        /// 不透明程度
+        /// </summary>
+        public double OpacityQuantity
+        {
+            get { return (double)GetValue(OpacityQuantityProperty); }
+            set { SetValue(OpacityQuantityProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OpacityQuantity.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OpacityQuantityProperty =
+            DependencyProperty.Register("OpacityQuantity", typeof(double), typeof(LoadingWait));
+
+
+
+        /// <summary>
+        /// 加载图标⚪的颜色
+        /// </summary>
+        public SolidColorBrush LoadPointColor
+        {
+            get { return (SolidColorBrush)GetValue(LoadPointColorProperty); }
+            set { SetValue(LoadPointColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadPointColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadPointColorProperty =
+            DependencyProperty.Register("LoadPointColor", typeof(SolidColorBrush), typeof(LoadingWait), new FrameworkPropertyMetadata(Brushes.Black));
+
+
+
+
+        /// <summary>
+        /// 加载显示的文字 
+        /// </summary>
+
+        public string LoadShowText
+        {
+            get { return (string)GetValue(LoadShowTextProperty); }
+            set { SetValue(LoadShowTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadShowText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadShowTextProperty =
+            DependencyProperty.Register("LoadShowText", typeof(string), typeof(LoadingWait), new FrameworkPropertyMetadata("loading"));
+
+
+        /// <summary>
+        /// 加载显示的文字颜色 默认值为 黑色
+        /// </summary>
+        public SolidColorBrush LoadShowTextColor
+        {
+            get { return (SolidColorBrush)GetValue(LoadShowTextColorProperty); }
+            set { SetValue(LoadShowTextColorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadShowTextColor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadShowTextColorProperty =
+            DependencyProperty.Register("LoadShowTextColor", typeof(SolidColorBrush), typeof(LoadingWait),new  FrameworkPropertyMetadata(Brushes.Black));
+
+
+
+        /// <summary>
+        /// 加载图标大小
+        /// </summary>
+        public int LoadSize
+        {
+            get { return (int)GetValue(LoadSizeProperty); }
+            set { SetValue(LoadSizeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LoadSize.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LoadSizeProperty =
+            DependencyProperty.Register("LoadSize", typeof(int), typeof(LoadingWait), new FrameworkPropertyMetadata(50));
+
+
+
+
+
+        #endregion
+
+    }
+}
--- Demo/demo1.xaml.cs
+++ Demo/demo1.xaml.cs
@@ -8,8 +8,10 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Demo
 {
@@ -89,7 +91,104 @@ namespace Demo
                  }
             };
             DataContext = _task;
+
+            DoubleAnimation da = new DoubleAnimation();
+            da.From = 0;
+            da.To = 360;
+            da.Duration = new Duration(TimeSpan.FromSeconds(3));
+            da.RepeatBehavior = RepeatBehavior.Forever;
+            RotateTransform rt = new RotateTransform();
+            runImg.RenderTransform = rt;
+            rt.BeginAnimation(RotateTransform.AngleProperty, da);
+
+            LoadingWait();
         }
+
+
+
+
+        public DispatcherTimer animationTimer;
+
+        public void LoadingWait()
+        {
+            animationTimer = new DispatcherTimer(
+               DispatcherPriority.ContextIdle, Dispatcher);
+            animationTimer.Interval = new TimeSpan(0, 0, 0, 0, 90);
+        }
+
+        private void Start()
+        {
+            animationTimer.Tick += HandleAnimationTick;
+            animationTimer.Start();
+        }
+
+        private void Stop()
+        {
+            animationTimer.Stop();
+            animationTimer.Tick -= HandleAnimationTick;
+        }
+
+        private void HandleAnimationTick(object sender, EventArgs e)
+        {
+            SpinnerRotate.Angle = (SpinnerRotate.Angle + 36) % 360;
+        }
+
+        private void HandleLoaded(object sender, RoutedEventArgs e)
+        {
+            const double offset = Math.PI;
+            const double step = Math.PI * 2 / 10.0;
+
+            SetPosition(C0, offset, 0.0, step);
+            SetPosition(C1, offset, 1.0, step);
+            SetPosition(C2, offset, 2.0, step);
+            SetPosition(C3, offset, 3.0, step);
+            SetPosition(C4, offset, 4.0, step);
+            SetPosition(C5, offset, 5.0, step);
+            SetPosition(C6, offset, 6.0, step);
+            SetPosition(C7, offset, 7.0, step);
+            SetPosition(C8, offset, 8.0, step);
+        }
+
+        private void SetPosition(Ellipse ellipse, double offset,
+            double posOffSet, double step)
+        {
+            ellipse.SetValue(Canvas.LeftProperty, 50.0
+                + Math.Sin(offset + posOffSet * step) * 50.0);
+
+            ellipse.SetValue(Canvas.TopProperty, 50
+                + Math.Cos(offset + posOffSet * step) * 50.0);
+        }
+
+        private void HandleUnloaded(object sender, RoutedEventArgs e)
+        {
+            Stop();
+        }
+
+        private void HandleVisibleChanged(object sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            bool isVisible = (bool)e.NewValue;
+            if (animationTimer == null)
+            {
+                LoadingWait();
+            }
+            if (isVisible)
+                Start();
+            else
+                Stop();
+        }
+
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._loading.Visibility = Visibility.Visible;
+        }
+
+        private void HideButton_Click(object sender, RoutedEventArgs e)
+        {
+            this._loading.Visibility = Visibility.Collapsed;
+        }
+
 
 
         void LoadData()
@@ -115,6 +214,11 @@ namespace Demo
 
             listbox.ItemsSource = listBoxItems;
             //  listbox3.ItemsSource = listBoxItems;
+        }
+
+        private void MediaElement_MediaEnded(object sender, RoutedEventArgs e)
+        {
+            ((MediaElement)sender).Position = ((MediaElement)sender).Position.Add(TimeSpan.FromMilliseconds(1));
         }
     }
 }
